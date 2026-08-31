0e0c36f0859cb6518ee2a335060d6a1207fa30ce
정민석(James) <minseok.jeong.james@gmail.com>
2026-08-31 22:57:08 +0900

    test: polyfill text codecs for PDF round-trip tests

--- src/testSetup.ts
+++ src/testSetup.ts
@@ -1,2 +1,10 @@
-import "fake-indexeddb/auto";
-import "@testing-library/jest-dom/vitest";
+import 'fake-indexeddb/auto';
+import '@testing-library/jest-dom/vitest';
+import { TextDecoder, TextEncoder } from 'node:util';
+
+if (!globalThis.TextEncoder) {
+  Object.defineProperty(globalThis, 'TextEncoder', { value: TextEncoder });
+}
+if (!globalThis.TextDecoder) {
+  Object.defineProperty(globalThis, 'TextDecoder', { value: TextDecoder });
+}
